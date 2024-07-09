5e1a1d51e80940ce75e01b801ac7f7150a91e836
BrownKnight <amanrk@hotmail.co.uk>
2024-07-09 19:17:43 +0100

    Remove widget extension for now

--- ovault/Preview Content/Extensions.swift
+++ ovault/Preview Content/Extensions.swift
@@ -26,35 +26,3 @@ extension View {
         try! ctx.save()
     }
 }
-
-extension OtpEntry {
-    static var testTotp30sec: OtpEntry = .init(
-        issuer: "Test Issuer",
-        accountName: "30sec Account",
-        algorithm: .SHA1,
-        digits: 6,
-        secret: "sharedsecret",
-        type: .totp,
-        counter: 0,
-        period: 30)
-    
-    static var testTotp15sec: OtpEntry = .init(
-        issuer: "Test Issuer",
-        accountName: "15sec Account",
-        algorithm: .SHA1,
-        digits: 6,
-        secret: "sharedsecret",
-        type: .totp,
-        counter: 0,
-        period: 15)
-    
-    static var testTotp60sec: OtpEntry = .init(
-        issuer: "Test Issuer",
-        accountName: "60sec 8digit Account",
-        algorithm: .SHA1,
-        digits: 8,
-        secret: "sharedsecret",
-        type: .totp,
-        counter: 0,
-        period: 60)
-}
