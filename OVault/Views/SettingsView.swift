cbdd69a386843b9fddd643101c232eb20d7fc6a7
BrownKnight <amanrk@hotmail.co.uk>
2024-10-23 18:30:25 +0100

    Add app build and developer info to the new Settings view

--- OVault/Views/SettingsView.swift
+++ OVault/Views/SettingsView.swift
@@ -2,6 +2,13 @@ import SwiftUI
 
 struct SettingsView: View {
     @AppStorage("iconsEnabled") private var iconsEnabled: Bool = false
+    
+    private var version: String {
+        Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String ?? "Unknown"
+    }
+    private var build: String {
+        Bundle.main.infoDictionary?["CFBundleVersion"] as? String ?? "Unknown"
+    }
 
     var body: some View {
         Form {
@@ -13,6 +20,20 @@ struct SettingsView: View {
 
                     If not enabled, no requests will be made to [Logo.dev](https://logo.dev).
                     """)
+            }
+            
+            Section("About") {
+                LabeledContent("Version", value: version)
+                LabeledContent("Build", value: build)
+                LabeledContent("GitHub") {
+                    Text("[JustAman62/ovault](https://github.com/JustAman62/ovault)")
+                }
+                LabeledContent("Website") {
+                    Text("[ovault.net](https://ovault.net)")
+                }
+                LabeledContent("Developer") {
+                    Text("[Aman Dhoot](https://amandhoot.com)")
+                }
             }
         }
         .formStyle(.grouped)
