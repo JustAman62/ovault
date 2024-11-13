e397b23ef98a6b8ea00daa7c3fc3530b439f317f
JustAman62 <amanrk@hotmail.co.uk>
2024-11-13 20:34:24 +0000

    Fix naming issue

--- Models/Services/Logger+Init.swift
+++ Models/Services/Logger+Init.swift
@@ -3,10 +3,10 @@ import OSLog
 
 public extension Logger {
     init<Subject>(_ subject: Subject) {
-        self.init(subsystem: Bundle.main.bundleIdentifier ?? "Roam.Andy", category: String(describing: subject))
+        self.init(subsystem: Bundle.main.bundleIdentifier ?? "OVault", category: String(describing: subject))
     }
     
     init(_ category: String) {
-        self.init(subsystem: Bundle.main.bundleIdentifier ?? "Roam.Andy", category: category)
+        self.init(subsystem: Bundle.main.bundleIdentifier ?? "OVault", category: category)
     }
 }
