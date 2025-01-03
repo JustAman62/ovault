f46c4b1893e240f13ffad5e5d52bcada0aae04eb
JustAman62 <amanrk@hotmail.co.uk>
2025-01-03 23:57:08 +0000

    Use a small default window size that suits the size of the content, and enforce a minimum size. Relates to #4.

--- OVault/ovaultApp.swift
+++ OVault/ovaultApp.swift
@@ -10,13 +10,17 @@ struct OVaultApp: App {
         WindowGroup("OVault", id: "otp-list") {
             ContentView()
                 .withNotifierSupport()
+                .frame(minWidth: 300, minHeight: 100)
         }
+        .windowResizability(.contentMinSize)
+        .defaultSize(width: 500, height: 350)
         .floatWindowIfSupportedAndEnabled()
         
 #if os(macOS)
         Settings {
             SettingsView()
         }
+        .windowResizability(.contentMinSize)
         .floatWindowIfSupportedAndEnabled()
         
         MenuBarExtra("OVault", image: "MenuBar", isInserted: $showMenuBarButton) {
